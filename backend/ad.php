b64c7b44b3f7e098bf7a2829d8f2fd35d5c6a96a
Sylvester Chen <sylvesterexba@gmail.com>
2021-08-10 10:19:06 +0800

    加入html版履歷修改成php副檔名並完成導覽列後台連接

--- backend/ad.php
+++ backend/ad.php
@@ -1,56 +1,55 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli"><?=$ts[$do];?></p>
-    
-    <form method="post" action="api/edit.php">
-        <table width="100%" class="cent">
-            <tbody>
-                <tr class="yel">
-                    <td width="80%">替代文字</td>
-                    <td width="10%">顯示</td>
-                    <td width="10%">刪除</td>
-                </tr>
+  <p class="t cent botli"><?=$ts[$do];?></p>
 
-                <?php
+  <form method="post" action="api/edit.php">
+    <table width="100%" class="cent">
+      <tbody>
+        <tr class="yel">
+          <td width="80%">替代文字</td>
+          <td width="10%">顯示</td>
+          <td width="10%">刪除</td>
+        </tr>
+
+        <?php
 
 $rows=$Ad->all();
 foreach ($rows as $key => $value) {
-?>
-<tr>
+    ?>
+        <tr>
 
-    <td>
-        <input type="text" name='text[]' value="<?=$value['text'];?>" style="width:90%">
-    </td>
-    <td>
-        <input type="checkbox" name="sh[]" value="<?=$value['id'];?>" <?=($value['sh']==1)?"checked":"";?>>
-    </td>
-    <td>
-        <input type="checkbox" name="del[]" value="<?=$value['id'];?>">    
-    </td>
+          <td>
+            <input type="text" name='text[]' value="<?=$value['text']; ?>" style="width:90%">
+          </td>
+          <td>
+            <input type="checkbox" name="sh[]" value="<?=$value['id']; ?>" <?=($value['sh']==1)?"checked":""; ?>>
+          </td>
+          <td>
+            <input type="checkbox" name="del[]" value="<?=$value['id']; ?>">
+          </td>
 
-    <input type="hidden" name="id[]" value="<?=$value['id'];?>">
-</tr>
-<?php
+          <input type="hidden" name="id[]" value="<?=$value['id']; ?>">
+        </tr>
+        <?php
 }
 ?>
-            </tbody>
-        </table>
-        <table style="margin-top:40px; width:70%;">
-            <tbody>
-                <tr>
-                    <td width="200px">
-                        <input type="button"
-                            onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?=$do;?>.php&#39;)" 
-                              value="<?=$as[$do];?>">
-                    </td>
-                    <td class="cent">
-                        <input type="submit" value="修改確定">
-                        <input type="reset" value="重置">
-                        <input type="hidden" name="table" value="<?=$do;?>">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      </tbody>
+    </table>
+    <table style="margin-top:40px; width:70%;">
+      <tbody>
+        <tr>
+          <td width="200px">
+            <input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?=$do;?>.php&#39;)"
+              value="<?=$as[$do];?>">
+          </td>
+          <td class="cent">
+            <input type="submit" value="修改確定">
+            <input type="reset" value="重置">
+            <input type="hidden" name="table" value="<?=$do;?>">
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-    </form>
+  </form>
 </div>
 </div>
---
+++ index01.php
@@ -0,0 +1,96 @@
+<?php include_once "base.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>我的線上履歷表</title>
+</head>
+<body>
+<div class="container">
+
+
+    <h1>線上履歷表</h1>
+
+<div class="row border-bottom p-5">
+    <div class="col-md-4" style='height:200px'>
+        <img src="img/<?=$Title->find(['sh'=>1])['img'];?>" class="h-100">
+    </div>
+    <div class="col-md-8">
+        <ul>
+        <?php
+            $myself=$Ad->all(['sh'=>1]);
+            foreach($myself as $my){
+        ?>
+            <li><?=$my['text'];?></li>
+
+        <?php        
+            }
+
+        ?>
+     
+        </ul>
+    </div>
+
+</div>
+
+<div class="row border-bottom pt-2 pb-5">
+    <h3>求職目標</h3><div class="col-md-8">
+        <ul>
+        <?php
+            $myself=$Ad->all(['sh'=>1]);
+            foreach($myself as $my){
+        ?>
+            <li><?=$my['text'];?></li>
+
+        <?php        
+            }
+
+        ?>
+     
+        </ul>
+    </div>
+    <h3>職稱</h3>
+    <div>求職描述或條件</div>
+</div>
+<div class="row border-bottom p-5">
+<h3>學經歷</h3>
+<div class="col-md-6">學歷</div>
+<div class="col-md-6">經歷</div>
+</div>
+<div class="row border-bottom p-5"></div>
+<div class="row border-bottom p-5 flex-wrap">
+<h3 class="w-100">作品集</h3>
+<?php
+$ports=$Image->all(['sh'=>1]);
+
+foreach($ports as $port){
+?>
+<div class="col-md-4 p-3" style="height:400px;">
+<a href="<?=$port['href'];?>">
+    <div class="shadow port-img border rounded-circle w-100" style="background:url('img/<?=$port['img'];?>');background-size:cover;height:300px">
+        
+    </div>
+</a>
+    <div>
+        <h3 class="port-subject" style="text-shadow:1px 1px 3px #ccc"><?=$port['subject'];?></h3>
+        <div class="port-descript"><?=$port['text'];?></div>
+    </div>
+</div>
+
+
+<?php
+}
+
+?>
+
+
+</div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
